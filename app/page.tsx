a6921a0b276ef166104f4bbdf631ec406b9aa830
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-08 23:23:38 +0500

    Added a child form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import TodoForm from "@/components/Form/todo-form";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Always be Progressive
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-screen">
+      <TodoForm />
     </main>
   );
 }
