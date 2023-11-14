dfc0e67f974ec0b8bd5a2b12939c2f04dbe105e1
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-14 20:53:45 +0500

    Added getTodos and delete Todos function

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,11 +1,28 @@
 'use server'
 
 import prisma from "@/lib/prismadb";
+import { Todo } from "@prisma/client";
+
+
+export const getTodoList = async (): Promise<Todo[]> => {
+    const todos = await prisma.todo.findMany();
+    return todos;
+}
 
 export const handleNewTodo = async (title: string) => {
-    await prisma.todo.create({
+    const newTodo = await prisma.todo.create({
         data: {
             title,
         },
     });
+    return newTodo;
+}
+
+export const deleteTodo = async (id: string) => {
+    const deletedTodo = await prisma.todo.delete({
+        where: {
+            id,
+        },
+    });
+    return deletedTodo;
 }
