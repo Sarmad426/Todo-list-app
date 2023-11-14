3170b347b8488d347eb0bc3ef23e36488109972d
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-14 19:23:08 +0500

    Removed unused imports

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,8 +1,6 @@
 'use server'
 
 import prisma from "@/lib/prismadb";
-import toast from "react-hot-toast";
-
 
 export const handleNewTodo = async (title: string) => {
     await prisma.todo.create({
