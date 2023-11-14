c36795ffe3de31acb1a237f1e75d8445df6350e9
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-14 19:22:15 +0500

    Added simple code

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,23 @@
-"use client";
-import Image from "next/image";
-// import trashIcon from "./delete.svg";
-import { useRouter } from "next/navigation";
-import { Trash, TrashIcon } from "lucide-react";
+import TodoForm from "@/components/Form/todo-form";
+import prisma from "@/lib/prismadb";
+import { Todo } from "@prisma/client";
+import React from "react";
 
-interface TodoProps {
-  title: string;
-  id: string;
-  complete: boolean;
-  toggleTodo: (id: string, complete: boolean) => void;
-  handleDelete: (id: string) => void;
-}
-const TodoItem = ({
-  id,
-  title,
-  complete,
-  toggleTodo,
-  handleDelete,
-}: TodoProps) => {
-  const router = useRouter();
+const getTodos = async (): Promise<Todo[]> => {
+  const todos = await prisma.todo.findMany();
+  return todos;
+};
+const Home = async () => {
+  const todos = await getTodos();
   return (
-    <div className="w-full m-5 px-2 grid grid-cols-2 gap-x-20 text-start lg:w-1/2 lg:text-center lg:mx-auto">
-      <section>
-        <input
-          type="checkbox"
-          className="peer cursor-pointer"
-          defaultChecked={complete}
-          id={id}
-          onChange={(e) => toggleTodo(id, e.target.checked)}
-        />
-        <label
-          htmlFor={id}
-          className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500 mx-2 text-sm lg:text-lg"
-        >
-          {title}
-        </label>
-      </section>
-      <section>
-        <button
-          className="mx-5 rounded-md py-1 px-2 bg-gray-100 hover:bg-gray-300"
-          onClick={(e) => {
-            handleDelete(id);
-            return router.refresh();
-          }}
-        >
-          <TrashIcon />
-        </button>
-      </section>
-    </div>
+    <main>
+      <TodoForm />
+      <div className="flex items-center justify-center flex-col">
+        <h3 className="text-3xl font-semibold">Tasks Here</h3>
+        <div className="my-6">{todos.map((todo) => todo.title)}</div>
+      </div>
+    </main>
   );
 };
 
-export default TodoItem;
+export default Home;
