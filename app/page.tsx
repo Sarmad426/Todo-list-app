39d4ed94cc7d313953a17a6611f919f33ad74d7e
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-14 20:54:28 +0500

    imported data from server actions file

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
+"use server";
+
+import { getTodoList } from "@/actions/actions";
 import TodoForm from "@/components/Form/todo-form";
 import Todos from "@/components/todos";
 import prisma from "@/lib/prismadb";
 import { Todo } from "@prisma/client";
 import React from "react";
 
-const getTodos = async (): Promise<Todo[]> => {
-  const todos = await prisma.todo.findMany();
-  return todos;
-};
 const Home = async () => {
-  const todos = await getTodos();
+  const todos = await getTodoList();
   return (
     <main>
       <TodoForm />
-      <Todos />
+      <Todos todos={todos} />
     </main>
   );
 };
