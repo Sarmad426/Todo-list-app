55ba66627393a8707fb1b03d58d411f38e38e1c0
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-15 16:25:47 +0500

    Completed and pending tasks lists

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
-import { getTodoList } from "@/actions/actions";
+import { getCompletedTodo, getTodoList } from "@/actions/actions";
 import TodoForm from "@/components/Form/todo-form";
 import Todos from "@/components/todos";
-import prisma from "@/lib/prismadb";
-import { Todo } from "@prisma/client";
-import React from "react";
 
 export const revalidate = 0;
 
 const Home = async () => {
   const todos = await getTodoList();
+  const completedTodos = await getCompletedTodo();
   return (
     <main>
       <TodoForm />
-      <Todos todos={todos} />
+      <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 w-screen">
+        <div className="w-full">
+          <h3 className="text-2xl font-semibold text-center text-gray-700">
+            Pending Tasks
+          </h3>
+          <Todos todos={todos} />
+        </div>
+        <div className="w-full">
+          <h3 className="text-2xl font-semibold text-center text-gray-700">
+            Completed Tasks
+          </h3>
+          <Todos todos={completedTodos} />
+        </div>
+      </div>
     </main>
   );
 };
