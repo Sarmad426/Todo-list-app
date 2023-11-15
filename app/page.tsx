1fefbac6dbb5f12e01dbb56f51102a90a6331c7c
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-15 18:02:52 +0500

    Adjusted todos width and removed scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ const Home = async () => {
   return (
     <main>
       <TodoForm />
-      <div className="flex items-center justify-center gap-12 flex-wrap">
-        <div className="lg:w-1/3 md:w-2/5 w-[95%] py-4 px-2 overflow-scroll no-scrollbar">
+      <div className="flex items-center justify-center gap-16 flex-wrap">
+        <div className="lg:w-1/3 md:w-2/5 w-[95%] p-6 overflow-scroll no-scrollbar border h-[25rem]">
           <h3 className="text-2xl font-semibold text-center text-gray-700">
             Pending Tasks
           </h3>
           <Todos todos={todos} />
         </div>
-        <div className="lg:w-1/3 md:w-2/5 w-[95%] py-4 px-2 overflow-scroll no-scrollbar">
+        <div className="lg:w-1/3 md:w-2/5 w-[95%] p-6 overflow-scroll no-scrollbar border h-[25rem]">
           <h3 className="text-2xl font-semibold text-center text-gray-700">
             Completed Tasks
           </h3>
