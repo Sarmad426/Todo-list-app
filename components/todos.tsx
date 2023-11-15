0924124d24427bfd6fd1459df439afe6da55efcc
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-15 17:11:08 +0500

    Added toggle todo function

--- components/todos.tsx
+++ components/todos.tsx
@@ -1,39 +1,17 @@
 "use client";
-import { deleteTodo, updateTodo } from "@/actions/actions";
+
 import TodoItem from "@/components/todoitem";
 import { Todo } from "@prisma/client";
-import toast from "react-hot-toast";
 
 interface TodosProps {
   todos: Todo[];
 }
 export default function Todos({ todos }: TodosProps) {
-  function toggleTodo(id: string, completed: boolean) {
-    try {
-      updateTodo(id, completed);
-    } catch {
-      toast.error("Something went wrong");
-    }
-  }
-  async function handleDelete(id: string) {
-    try {
-      deleteTodo(id);
-    } catch {
-      toast.error("Something went wrong");
-    }
-  }
   return (
     <main className="text-center m-auto my-12">
       {todos.length > 0 ? (
         todos.map((todo: Todo) => {
-          return (
-            <TodoItem
-              key={todo.id}
-              {...todo}
-              toggleTodo={toggleTodo}
-              handleDelete={handleDelete}
-            />
-          );
+          return <TodoItem key={todo.id} {...todo} />;
         })
       ) : (
         <p className="my-8 text-lg text-muted-foreground">No Tasks</p>
