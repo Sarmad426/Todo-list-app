a1866cee39b06ee76d6bb9c066ed7f1d487e7128
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-16 16:32:51 +0500

    Added a link to view todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import TodoForm from "@/components/Form/todo-form";
 import Image from "next/image";
 import logo from "../public/logo.png";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { EyeIcon } from "lucide-react";
 
 const Home = async () => {
   return (
     <main>
-      <div className="mt-3 mb-5 flex items-center justify-center">
+      <div className="mt-3 mb-5 flex items-center justify-center gap-x-32">
         <Image src={logo} alt="Logo" className="w-16 rounded-full" />
+        <Link href="/todos" className={buttonVariants()}>
+          View Todos <EyeIcon className="ml-2" />
+        </Link>
       </div>
       <TodoForm />
     </main>
