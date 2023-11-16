180f11d1f9bb9459a5434c5488f74e08eb74e7fe
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-16 15:04:25 +0500

    Added logo on the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getCompletedTodo, getTodoList } from "@/actions/actions";
 import TodoForm from "@/components/Form/todo-form";
 import Todos from "@/components/todos";
+import Image from "next/image";
+import logo from "../public/logo.png";
 
 export const revalidate = 0;
 
@@ -9,6 +11,9 @@ const Home = async () => {
   const completedTodos = await getCompletedTodo();
   return (
     <main>
+      <div className="mt-3 mb-5 flex items-center justify-center">
+        <Image src={logo} alt="Logo" className="w-16 rounded-full" />
+      </div>
       <TodoForm />
       <div className="flex items-center justify-center gap-16 flex-wrap">
         <div className="lg:w-1/3 md:w-2/5 w-[95%] p-6 overflow-scroll no-scrollbar border h-[25rem]">
