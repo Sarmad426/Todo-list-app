cddbd0be2e8e59377827398b1ea0315657ad5f3f
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-16 16:02:56 +0500

    Completely Revamped the todo form

--- components/Form/todo-form.tsx
+++ components/Form/todo-form.tsx
@@ -1,56 +1,78 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { SendHorizonal } from "lucide-react";
-import { handleNewTodo } from "@/actions/actions";
-import { FormEvent, useState } from "react";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { handleNewTodo } from "@/actions/actions";
 import toast from "react-hot-toast";
 
-const TodoForm = () => {
-  const [title, setTitle] = useState("");
-  const [loading, setLoading] = useState(false);
+type TodoFormProps = {
+  onAddTodo: (text: string) => void;
+};
+
+type FormInputs = {
+  todo: string;
+};
+
+const TodoForm: React.FC<TodoFormProps> = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
-  const handleTodo = (e: FormEvent) => {
-    e.preventDefault();
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
     try {
       setLoading(true);
-      handleNewTodo(title);
+      handleNewTodo(data.todo);
       router.refresh();
       toast.success("Todo added successfully");
-      setTitle("");
     } catch (error) {
       console.log(error);
       toast.error("Something went wrong.");
     } finally {
+      reset();
       setLoading(false);
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center my-6 py-4">
+    <div className="max-w-md mx-auto my-8 px-4 py-8 border rounded shadow">
       <h2 className="text-3xl font-semibold my-4">Todo App</h2>
       <p className="my-3 text-muted-foreground">
         Be Productive by managing tasks efficiently.
       </p>
-      <form
-        onSubmit={handleTodo}
-        className="flex items-center justify-center w-full gap-6 my-6"
-      >
-        <Input
-          type="text"
-          name="todo"
-          placeholder="New Task Here..."
-          className="w-full lg:w-1/3 md:w-1/2 disabled:opacity-50"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          disabled={loading}
-          required
-        />
-        <Button size="sm" disabled={loading} className="disabled:opacity-50">
-          <SendHorizonal />
-        </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex items-center gap-4">
+          <input
+            type="text"
+            id="todoText"
+            {...register("todo", { required: "Todo is required" })}
+            className={`flex-1 p-2 border rounded-l focus:outline-none disabled:opacity-60 ${
+              errors.todo ? "border-red-500" : "border-gray-300"
+            }`}
+            placeholder="Enter your todo..."
+            disabled={loading}
+          />
+
+          <Button
+            size="sm"
+            type="submit"
+            disabled={loading}
+            className="disabled:opacity-60"
+          >
+            <SendHorizonal size={20} />
+          </Button>
+        </div>
+        <p className="text-red-500 text-xs mt-1">
+          {Boolean(errors.todo) && errors?.todo?.message}
+        </p>
       </form>
     </div>
   );
